587b4cc345c8545591cfbfd9f0660908fc5e114d
Maupertuisbug <researchj09@gmail.com>
2025-07-24 13:45:21 +0000

    correct the nohup command

--- env.sh
+++ env.sh
@@ -11,3 +11,4 @@ export PYTHONPATH=$PWD
 # cd $CONDA_PREFIX/bin
 # ln -s x86_64-conda_cos7-linux-gnu-gcc gcc
 # source anaconda3/etc/profile.d/conda.sh
+# nohup python generate_dataset/dqnAgent.py -config config/dqn_dataset.yml  > output.log 2>&1 &
